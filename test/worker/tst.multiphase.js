1e7857b937023ae14a4bc006dc524a1ef10ea9ec
Dave Pacheco <dap@joyent.com>
2012-06-07 16:52:14 -0700

    MANTA-8 add support for multi-phase jobs

--- test/worker/tst.multiphase.js
+++ test/worker/tst.multiphase.js
@@ -1,5 +1,5 @@
 /*
- * tst.basic.js: runs a basic job through the worker
+ * tst.multiphase.js: runs a multi-phase job through the worker
  */
 
 var mod_assert = require('assert');
@@ -18,9 +18,11 @@ mod_vasync.pipeline({
 	setup,
 	submitJob,
 	checkJob,
-	checkTaskGroups,
-	updatePartial,
-	updateRest,
+	checkPhase1,
+	finishPhase1,
+	checkPhase2,
+	finishPhase2,
+	checkDone,
 	teardown
     ]
 }, function (err) {
@@ -39,7 +41,6 @@ function setup(_, next)
 {
 	log.info('setup');
 
-	taskgroups = {};
 	moray = mod_worklib.createMoray();
 	worker = mod_worklib.createWorker({ 'moray': moray });
 	worker.start();
@@ -52,7 +53,7 @@ function submitJob(_, next)
 
 	moray.put(bktJobs, 'job-001', {
 	    'jobId': 'job-001',
-	    'phases': [ {} ],
+	    'phases': [ {}, {} ],
 	    'inputKeys': [ 'key1', 'key2', 'key3', 'key4' ]
 	});
 
@@ -62,24 +63,28 @@ function submitJob(_, next)
 function checkJob(_, next)
 {
 	log.info('checkJob');
+
 	mod_worklib.timedCheck(10, 1000, function (callback) {
-		var job = moray.get(bktJobs, 'job-001');
-		mod_assert.equal('worker-000', job['worker']);
-		mod_assert.equal('running', job['state']);
+		mod_assert.equal('worker-000',
+		    moray.get(bktJobs, 'job-001')['worker']);
 		callback();
 	}, next);
 }
 
-function checkTaskGroups(_, next)
+function checkPhase1(_, next)
 {
-	log.info('checkTaskGroups');
+	log.info('checkPhase1');
+
 	mod_worklib.timedCheck(10, 1000, function (callback) {
 		var groups = moray.list(bktTgs).map(
 		    moray.get.bind(moray, bktTgs));
-		mod_assert.equal(3, groups.length);
-		log.info('found groups', groups);
 
-		var hosts = {}, keys = {};
+		var count = 0;
+
+		mod_assert.equal(moray.get(bktJobs, 'job-001')['state'],
+		    'running');
+
+		taskgroups = {};
 
 		groups.forEach(function (group) {
 			mod_assert.equal('job-001', group['jobId']);
@@ -87,93 +92,105 @@ function checkTaskGroups(_, next)
 			mod_assert.ok(0 === group['phaseNum']);
 			mod_assert.deepEqual([], group['results']);
 
-			group['inputKeys'].forEach(function (key) {
-				mod_assert.ok(!keys.hasOwnProperty(key));
-				keys[key] = true;
-			});
-
-			mod_assert.ok(!hosts.hasOwnProperty(group['host']));
-			hosts[group['host']] = true;
-
 			mod_assert.ok(!taskgroups.hasOwnProperty(
 			    group['taskGroupId']));
 			taskgroups[group['taskGroupId']] = group;
+			count++;
 		});
 
-		mod_assert.deepEqual(hosts,
-		    { 'node0': true, 'node1': true, 'node2': true });
-
-		mod_assert.deepEqual(keys,
-		    { 'key1': true, 'key2': true, 'key3': true, 'key4': true });
+		mod_assert.equal(count, 3);
 
 		callback();
 	}, next);
 }
 
-function updatePartial(_, next)
+function finishPhase1(_, next)
 {
-	var group, key;
-
-	log.info('updatePartial');
-
-	/*
-	 * Report partial completion for one of the task groups.
-	 */
-	for (key in taskgroups) {
-		group = taskgroups[key];
-
-		group['results'].push({
-		    'result': 'ok',
-		    'input': group['inputKeys'][0],
-		    'outputs': [ 'result1' ]
-		});
-
-		moray.put(bktTgs, key, group);
-		break;
-	}
-
-	mod_worklib.timedCheck(10, 1000, function (callback) {
-		var job = moray.get(bktJobs, 'job-001');
-
-		mod_assert.equal(job['jobId'], 'job-001');
-		mod_assert.deepEqual(job['phases'], [ {} ]);
-		mod_assert.deepEqual(job['inputKeys'],
-		    [ 'key1', 'key2', 'key3', 'key4' ]);
-
-		mod_assert.deepEqual(job['outputKeys'], [ 'result1' ]);
-
-		mod_assert.ok(worker.mw_jobs.hasOwnProperty('job-001'));
-
-		callback();
-	}, next);
-}
-
-function updateRest(_, next)
-{
-	log.info('updateRest');
+	log.info('finishPhase1');
 
 	mod_jsprim.forEachKey(taskgroups, function (tgid, group) {
 		group['results'] = group['inputKeys'].map(function (key) {
 			return ({
 			    'result': 'ok',
 			    'input': key,
-			    'outputs': []
+			    'outputs': [ '2' + key + '1', '2' + key + '2' ]
 			});
 		});
 
 		moray.put(bktTgs, tgid, group);
 	});
 
+	next();
+}
+
+function checkPhase2(_, next)
+{
+	log.info('checkPhase2');
+
+	mod_worklib.timedCheck(10, 1000, function (callback) {
+		var groups = moray.list(bktTgs).map(
+		    moray.get.bind(moray, bktTgs));
+		var count = 0;
+
+		mod_assert.equal(moray.get(bktJobs, 'job-001')['state'],
+		    'running');
+
+		taskgroups = {};
+
+		groups.forEach(function (group) {
+			if (group['phaseNum'] === 0)
+				return;
+
+			mod_assert.equal('job-001', group['jobId']);
+			mod_assert.equal('dispatched', group['state']);
+			mod_assert.ok(1 === group['phaseNum']);
+			mod_assert.deepEqual([], group['results']);
+
+			mod_assert.ok(!taskgroups.hasOwnProperty(
+			    group['taskGroupId']));
+			taskgroups[group['taskGroupId']] = group;
+			count++;
+		});
+
+		mod_assert.equal(count, 3);
+
+		callback();
+	}, next);
+}
+
+function finishPhase2(_, next)
+{
+	log.info('finishPhase2');
+
+	mod_jsprim.forEachKey(taskgroups, function (tgid, group) {
+		group['results'] = group['inputKeys'].map(function (key) {
+			return ({
+			    'result': 'ok',
+			    'input': key,
+			    'outputs': [ '3' + key ]
+			});
+		});
+
+		moray.put(bktTgs, tgid, group);
+	});
+
+	next();
+}
+
+function checkDone(_, next)
+{
 	mod_worklib.timedCheck(10, 1000, function (callback) {
 		var job = moray.get(bktJobs, 'job-001');
 
 		mod_assert.equal(job['jobId'], 'job-001');
 		mod_assert.equal(job['state'], 'done');
-		mod_assert.deepEqual(job['phases'], [ {} ]);
+		mod_assert.deepEqual(job['phases'], [ {}, {} ]);
 		mod_assert.deepEqual(job['inputKeys'],
 		    [ 'key1', 'key2', 'key3', 'key4' ]);
 
-		mod_assert.deepEqual(job['outputKeys'], [ 'result1' ]);
+		mod_assert.deepEqual(job['outputKeys'].sort(), [
+		    '32key11', '32key12', '32key21', '32key22',
+		    '32key31', '32key32', '32key41', '32key42' ]);
 
 		mod_assert.ok(!worker.mw_jobs.hasOwnProperty('job-001'));
 
