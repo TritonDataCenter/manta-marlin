7ee66dc6c7f0a0ebc0be3ee083c17fd009d48dde
Dave Pacheco <dap@joyent.com>
2012-09-25 18:06:58 -0700

    MANTA-312 marlin.git/env.sh puts Marlin's node and the *end* of the PATH

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 # bash environment file for working on Marlin
 #
 
-export PATH="$PATH:$PWD/build/node/bin:$PWD/cmd:$PWD/tools"
-export PATH="$PATH:$PWD/node_modules/moray/bin"
+export PATH="$PWD/build/node/bin:$PWD/cmd:$PWD/tools:$PATH"
+export PATH="$PWD/node_modules/moray/bin:$PATH"
