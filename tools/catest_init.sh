ce9c745d032c7b3afb6e432e41eb91b9c51301f2
Dave Pacheco <dap@joyent.com>
2013-04-03 13:59:30 -0700

    MANTA-1116 marlin should record data for compute metering

--- tools/catest_init.sh
+++ tools/catest_init.sh
@@ -2,9 +2,97 @@
 # catest_init.sh: Marlin-specific catest initialization file
 #
 
+agentconfig="/opt/smartdc/marlin/etc/agentconfig.json"
+
+#
+# resolve resolver_ip url: Return $url with the hostname part resolved using
+# $resolver_ip as a DNS resolver.  On error, returns $url unmodified.
+#
+function resolve
+{
+	local url=$2
+	local protocol
+	local portsuffix
+	local host
+	local rv
+
+	protocol=${url%%://*}
+	portsuffix=${url##*:}
+	if [[ -z "$protocol" || "$protocol" == "$url" || \
+	    -z "$portsuffix" || "$portsuffix" == "$url" ]]; then
+		echo "$url"
+		return
+	fi
+
+	# First trim the leading "protocol" plus "://"
+	host=${url:$(( 3 + ${#protocol}))}
+	# Next trim the trailing $portsuffix plus ":"
+	host=${host:0: $(( ${#host} - ${#portsuffix} - 1))}
+
+	if [[ -z "$host" || "$host" == "$url" ]]; then
+		echo "$url"
+		return
+	fi
+
+	rv=$(set -o pipefail; \
+	    host -t A "$host" $1 | tail -1 | awk '{print $4}') || rv=
+	if [[ -z "$rv" ]]; then
+		echo "$url"
+		return
+	fi
+
+	echo "$protocol://$rv:$portsuffix"
+}
+
+#
+# If we're in the global zone and there's an agent configuration file present,
+# we initialize our own configuration from that one.
+#
+function catest_init_global
+{
+	[[ -f $agentconfig ]] || return 1
+
+	echo -n "Attempting to configure based on GZ agent configuration ... "
+
+	local nameserver=$(json dns.nameservers.0 < $agentconfig)
+	[[ -n "$nameserver" ]] || return 1
+
+	[[ -z $MANTA_URL ]] &&
+	    export MANTA_URL=$(resolve $nameserver \
+	        $(json manta.url < $agentconfig) | sed -e s'#^http:#https:#' | \
+		sed -Ee s'#:80/?$##')
+	[[ -z $MORAY_URL ]] &&
+	    export MORAY_URL=$(resolve $nameserver \
+	        $(json moray.url < $agentconfig))
+	[[ -z $MANTA_USER ]] && export MANTA_USER="poseidon"
+	[[ -z $MANTA_KEY_ID ]] && export MANTA_KEY_ID="$(sdc-ldap search -b \
+	    "$(sdc-ldap search login=poseidon | head -1 | cut -c5-)" \
+	    objectclass=sdckey | awk '/^fingerprint:/{ print $2 }')"
+
+	#
+	# The mahi URL is trickier because it's not actually in the agent
+	# configuration.  But we can guess it by replacing the first component
+	# of the unresolved $MANTA_URL with "auth".
+	#
+	[[ -z $MAHI_URL ]] &&
+	    export MAHI_URL=$(resolve $nameserver \
+		$(json manta.url < $agentconfig | sed -Ee \
+		s'#https?://[a-zA-Z0-9]+([^:/]*)([:/].*)?#tcp://auth\1:6379#'))
+
+	echo "done."
+}
+
 function catest_init
 {
 	nfailed=0
+
+	if [[ $(zonename) == "global" ]]; then
+		catest_init_global || \
+		    echo "WARN: failed to configure from $agentconfig" >&2
+	else
+		echo "NOTE: skipping metering tests because you're running" \
+		    "in a non-global zone." >&2
+	fi
 
 	if [[ -z "$MAHI_URL" ]]; then
 		((nfailed++))
